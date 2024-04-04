64d82566b40b3064635a995bcccd2213b80a5b8c
OlivierLartillot <olivier.lartillot@gmail.com>
2024-04-04 21:48:55 +0200

    suppression de la derniere migration - regroupement des instructions dans la précédente

--- migrations/Version20240404140446.php
+++ migrations/Version20240404140446.php
@@ -22,6 +22,7 @@ final class Version20240404140446 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE licence_comment ADD created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
         $this->addSql('ALTER TABLE licence_comment ADD CONSTRAINT FK_5AC398DFA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE licence_comment ADD updated_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
         $this->addSql('CREATE INDEX IDX_5AC398DFA76ED395 ON licence_comment (user_id)');
     }
 
@@ -31,5 +32,6 @@ final class Version20240404140446 extends AbstractMigration
         $this->addSql('ALTER TABLE licence_comment DROP FOREIGN KEY FK_5AC398DFA76ED395');
         $this->addSql('DROP INDEX IDX_5AC398DFA76ED395 ON licence_comment');
         $this->addSql('ALTER TABLE licence_comment DROP created_at');
+        $this->addSql('ALTER TABLE licence_comment DROP updated_at');
     }
 }
